add000ae53bfaaf8d65c1b9265d842a566cd7e23
VRamazing <r.vignesh149@gmail.com>
2017-10-03 02:18:48 +0530

    Game Reload

--- js/index.js
+++ js/index.js
@@ -2,23 +2,24 @@ $(document).ready(function(){
     // var arr = [];
     var present,previous;
     var flag = true;
+    var tiles = 0;
     $(".back").hover(function(){
         var present = $(this).attr("name");
         var parentId =  $(this).parent().attr("id");
         
-       
         if(present == previous + '2' || previous == present + '2' ){
            
            $("[id=" + parentId + "]").css("visibility", "hidden");
+           tiles += 2
            
+        }
+        previous = present;
 
+        if(tiles > 0 ){
+          $(".wrapper").html("<center class = 'youwin'><h1>You win</h1><p>Refreshing Game</p><i class='fa fa-refresh fa-spin fa-3x'></i></center>");
+
+          setTimeout(function(){location.reload();},4000);
         }
 
-
-       previous = present;
-       
-        
-
-        
     });
 });
