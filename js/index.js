38c18c6a9eedde1013044b6536170a2303a69025
Martishevich Oleg <oleg.martishevich@gmail.com>
2017-10-31 00:32:25 +0300

    js and css style refactoring

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 $(document).ready(function(){
-    // var arr = [];
-    var present,previous;
+    var present, previous;
     var flag = true;
     var tiles = 0;
-    $(".back").hover(function(){
+    $(".back").hover(function() {
         var present = $(this).attr("name");
         var parentId =  $(this).parent().attr("id");
         
-        if(present == previous + '2' || previous == present + '2' ){
-           
+        if (present == previous + '2' || previous == present + '2') {
            $("[id=" + parentId + "]").css("visibility", "hidden");
-           tiles += 2
-           
+           tiles += 2;
         }
         previous = present;
-
-        if(tiles == 40  ){
-          $(".wrapper").html("<center class = 'youwin'><h1>You win</h1><p>Refreshing Game</p><i class='fa fa-refresh fa-spin fa-3x'></i></center>");
-
-          setTimeout(function(){location.reload();},4000);
+        if (tiles == 40) {
+          $(".wrapper").html("<center class = 'youwin'><h1>You win</h1>" + 
+            "<p>Refreshing Game</p>" + 
+            "<i class='fa fa-refresh fa-spin fa-3x'></i></center>");
+          setTimeout(
+            function() {
+              location.reload(); 
+            }, 
+            4000
+          );
         }
-
     });
 });
